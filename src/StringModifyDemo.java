0804449c09b3ba6d407711310f212f638aac0621
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-15 12:22:56 +0400

    Task 3: Search and Modify Text Using Java String Methods

--- src/StringModifyDemo.java
+++ src/StringModifyDemo.java
@@ -5,6 +5,7 @@ public class StringModifyDemo {
 
         //trim() method to remove leading and trailing spaces
         System.out.println(str.trim());
+        //contains() method to check whether the string contains a specific word
         System.out.println("Is the string have a:" +str.contains("a"));
 
         // replace() method to change one word in the string
