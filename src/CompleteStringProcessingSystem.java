3cfde0409ade5079e9ea3b43a5d6861d1910338c
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-16 09:02:26 +0400

    Task 25: Update vowels check condition

--- src/CompleteStringProcessingSystem.java
+++ src/CompleteStringProcessingSystem.java
@@ -33,7 +33,7 @@ public class CompleteStringProcessingSystem {
 
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'||ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
                 vowelCount++;
             }
         }
